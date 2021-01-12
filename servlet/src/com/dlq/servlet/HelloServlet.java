5930c5dadcf2db30fe07040e51c6ecb3f2334e6e
D·Teach <1114626450@qq.com>
2021-01-12 23:50:49 +0800

    Servlet案例

---
+++ servlet/src/com/dlq/servlet/HelloServlet.java
@@ -0,0 +1,79 @@
+package com.dlq.servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ *@program: Java_Web
+ *@description:
+ *@author: Hasee
+ *@create: 2021-01-12 20:25
+ */
+public class HelloServlet implements Servlet {
+
+    public HelloServlet() {
+        System.out.println("1--->  执行构造器方法");
+    }
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        System.out.println("2--->  执行init初始化方法");
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    /**
+     * service方法是专门用来处理请求和响应的
+     * @param servletRequest
+     * @param servletResponse
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse)
+            throws ServletException, IOException {
+        System.out.println("3---->  执行service方法 == Hello Servlet!!! " +
+                "Tomcat的"+servletRequest.getClass()+"类实现了ServletRequest子接口HttpServletRequest");
+        //类型转换(因为它有getMethod()方法)
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        //获取请求的方式
+        String method = httpServletRequest.getMethod();
+        if ("GET".equals(method)){
+            doGet();
+        }else if("POST".equals(method)){
+            doPost();
+        }
+    }
+
+    /**
+     * 做get请求操作
+     */
+    public void doGet(){
+        System.out.println("get---请求");
+        System.out.println("get---请求aaaa");
+        System.out.println("get---请求aaaaaa");
+    }
+
+    /**
+     * 做post请求操作
+     */
+    public void doPost(){
+        System.out.println("post---请求");
+        System.out.println("post---请求aa");
+        System.out.println("post---请求aaaa");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("4--->  执行destroy销毁方法");
+    }
+}
